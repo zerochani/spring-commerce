7300b510733430d3a1e40ed835f2da3ce088c4e9
CHAN <150508884+zerochani@users.noreply.github.com>
2025-09-25 00:02:36 +0900

    feat 프로젝트 초기 세팅완료

---
+++ src/main/java/com/example/commerce_mvp/CommerceMvpApplication.java
@@ -0,0 +1,13 @@
+package com.example.commerce_mvp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommerceMvpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommerceMvpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/commerce_mvp/CommerceMvpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.commerce_mvp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommerceMvpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
