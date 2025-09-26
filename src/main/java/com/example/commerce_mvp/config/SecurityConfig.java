e6b41943226d72029d918b12927592b3337e38af
CHAN <150508884+zerochani@users.noreply.github.com>
2025-09-26 23:03:48 +0900

    feat: configure OAuth2 login to use CustomOAuthUserService for user information

--- src/main/java/com/example/commerce_mvp/config/SecurityConfig.java
+++ src/main/java/com/example/commerce_mvp/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.example.commerce_mvp.config;
 
+import com.example.commerce_mvp.application.user.CustomOAuthUserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,7 +10,11 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final CustomOAuthUserService customOAuthUserService;
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         http
@@ -18,6 +24,9 @@ public class SecurityConfig {
                 )
                 .oauth2Login(oauth2->oauth2
                         .loginPage("/login")
+                        .userInfoEndpoint(userInfo -> userInfo
+                                .userService(customOAuthUserService)
+                        )
                 );
         return http.build();
     }
