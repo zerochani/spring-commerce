83f05a7074d2ebe21cbdd41fa65c7b2f15d195a8
CHAN <150508884+zerochani@users.noreply.github.com>
2025-10-03 14:40:08 +0900

    feat: add AccessTokenResponseDto for access token response handling

---
+++ src/main/java/com/example/commerce_mvp/application/auth/dto/AccessTokenResponseDto.java
@@ -0,0 +1,13 @@
+package com.example.commerce_mvp.application.auth.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class AccessTokenResponseDto {
+    private String accessToken;
+}
