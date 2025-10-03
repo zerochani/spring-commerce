89c61298a0bf6201ec881253409644ece47876f1
CHAN <150508884+zerochani@users.noreply.github.com>
2025-10-03 14:41:54 +0900

    feat: integrate OAuth2LoginSuccessHandler and remove HttpSession dependency in CustomOAuthUserService

--- src/main/java/com/example/commerce_mvp/application/user/CustomOAuthUserService.java
+++ src/main/java/com/example/commerce_mvp/application/user/CustomOAuthUserService.java
@@ -1,7 +1,6 @@
 package com.example.commerce_mvp.application.user;
 
 import com.example.commerce_mvp.application.user.dto.OAuthAttributes;
-import com.example.commerce_mvp.application.user.dto.SessionUser;
 import com.example.commerce_mvp.domain.user.User;
 import com.example.commerce_mvp.domain.user.UserRepository;
 import jakarta.servlet.http.HttpSession;
@@ -23,7 +22,6 @@ import java.util.Collections;
 public class CustomOAuthUserService implements OAuth2UserService<OAuth2UserRequest, OAuth2User> {
 
     private final UserRepository userRepository;
-    private final HttpSession httpSession;
 
     @Override
     @Transactional
@@ -31,11 +29,14 @@ public class CustomOAuthUserService implements OAuth2UserService<OAuth2UserReque
 
         OAuth2UserService<OAuth2UserRequest, OAuth2User> oAuth2UserService = new DefaultOAuth2UserService();
         OAuth2User oAuth2User = oAuth2UserService.loadUser(userRequest);
+
         String registrationId = userRequest.getClientRegistration().getRegistrationId();
         String userNameAttributeName = userRequest.getClientRegistration().getProviderDetails().getUserInfoEndpoint().getUserNameAttributeName();
+
         OAuthAttributes attributes = OAuthAttributes.of(registrationId, userNameAttributeName, oAuth2User.getAttributes());
+
         User user = saveOrUpdate(attributes);
-        httpSession.setAttribute("user", new SessionUser(user));
+
         return new DefaultOAuth2User(
                 Collections.singleton(new SimpleGrantedAuthority(user.getRole().getValue())),
                 attributes.getAttributes(),
--- src/main/java/com/example/commerce_mvp/config/SecurityConfig.java
+++ src/main/java/com/example/commerce_mvp/config/SecurityConfig.java
@@ -1,11 +1,13 @@
 package com.example.commerce_mvp.config;
 
 import com.example.commerce_mvp.application.user.CustomOAuthUserService;
+import com.example.commerce_mvp.application.user.OAuth2LoginSuccessHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -14,11 +16,12 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     private final CustomOAuthUserService customOAuthUserService;
+    private final OAuth2LoginSuccessHandler oAuth2LoginSuccessHandler;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         http
-                .csrf(csrf-> csrf.disable())
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorize -> authorize
                         .anyRequest().permitAll()
                 )
@@ -27,6 +30,7 @@ public class SecurityConfig {
                         .userInfoEndpoint(userInfo -> userInfo
                                 .userService(customOAuthUserService)
                         )
+                        .successHandler(oAuth2LoginSuccessHandler)
                 );
         return http.build();
     }
